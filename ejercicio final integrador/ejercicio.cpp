09fd02d40a33302e72f4142e653741544d308163
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 11:58:13 +0100

    2

--- ejercicio final integrador/ejercicio.cpp
+++ ejercicio final integrador/ejercicio.cpp
@@ -9,3 +9,25 @@ struct Entorno {
 
 };
 
+class Variant {
+public:
+    typedef Variant(*proc_type)(const std::vector<Variant>&);
+    typedef std::vector<Variant>::const_iterator iter;
+    typedef std::map<std::string, Variant> map;
+
+    variant_type type;
+    std::string val;
+    std::vector<Variant> list;
+    proc_type proc;
+    Entorno* env;
+
+    Variant(variant_type type = Symbol) : type(type), env(nullptr), proc(nullptr) {}
+    Variant(variant_type type, const std::string& val) : type(type), val(val), env(nullptr), proc(nullptr) {}
+    Variant(proc_type proc) : type(Proc), proc(proc), env(nullptr) {}
+
+    std::string to_string();
+    std::string to_json_string();
+    static Variant from_json_string(const std::string& json);
+    static Variant parse_json(jsonlib::Json job);
+    Variant apply(const std::vector<Variant>& args);
+    void setEnvironment(Entorno* environment);
